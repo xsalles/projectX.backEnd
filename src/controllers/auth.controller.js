6b503fd54b84addea5370b328e7980795703d547
xsalles <xs.salles@gmail.com>
2025-05-28 15:58:50 -0300

    feat: added the function to manage login.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv/config";
+import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+
+dotenv.config()
 
 const prisma = new PrismaClient();
 
@@ -10,10 +12,7 @@ export const register = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return {
-        status: 400,
-        message: "Email and password are required",
-      };
+      res.status(400).json({ message: "Email and password are required" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -27,13 +26,65 @@ export const register = async (req, res) => {
 
     res.status(201).json({
       message: "User registered successfully",
-      
     })
   } catch (error) {
-    console.error("Error registering user:", error);
-    return {
-      status: 500,
+    return res.status(500).json({
       message: "Internal server error",
-    };
+      error: error.message,
+    });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email}
+    })
+
+    if(!user) {
+      return res.status(404).json({ message: "User not found"})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const secret = process.env.JWT_SECRET_KEY;
+
+    console.log(`Byte length: ${Buffer.byteLength(secret, 'utf8') /2} bytes`);
+
+    const payload = {
+      userId: user.id,
+      createdAt: user.createdAt,
+    }
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      algorithm: "HS256",
+      expiresIn: "1h",
+    })
+
+    if (!token) {
+      return res.status(500).json({ message: "Failed to generate token" });
+    }
+
+    return res.status(200).json({
+      message: "Login successful",
+      token: token,
+      user: {
+        id: user.id,
+        email: user.email,
+        createdAt: user.createdAt
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error: error.message });
+  }
+}
