081f77305d658d02d6ef4f827ffa5aa524c9bd13
xsalles <xs.salles@gmail.com>
2025-06-01 11:44:19 -0300

    feat: I setted cors and corrected error that i was getting in front end

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import e from 'express';
 import 'dotenv/config'
 import { authRoutes } from './routes/auth.routes.js';
+import cors from 'cors'
 
 const app = e()
 
 app.use(e.json())
 app.use('/api/auth', authRoutes)
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 const PORT = process.env.PORT
 
